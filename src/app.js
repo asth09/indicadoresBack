618e981701795b4a72b99ab7169f16a610293657
asth09 <asthackerman@gmail.com>
2026-07-22 13:29:35 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./db.js";
 
 import authRoutes from './routes/auth.routes.js';
 import cedisRoutes from './routes/cedis.routes.js';
@@ -11,15 +10,12 @@ import usersRoutes from './routes/user.routes.js';
 // 1. OBLIGATORIO: Cargar las variables de entorno inmediatamente
 dotenv.config();
 
-// 2. Ahora sí ejecutamos la conexión (ya procesó el .env)
-connectDB();
-
 const app = express();
 
 app.use(cors({
     origin: [
     'http://localhost:5173',               // Tu entorno de desarrollo local
-    'https://indicadores-front.vercel.app/'       // URL real de tu Frontend en Vercel
+    'https://indicadores-front.vercel.app'       // URL real de tu Frontend en Vercel
   ],
     credentials: true, // Permitir credenciales
     methods: "PUT, POST, GET, DELETE, PATCH, OPTIONS",
